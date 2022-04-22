a5b0d06db1d7de27f4292032484d18fa71502eaa
Juanes07 <juanesdm3@gmail.com>
2022-04-21 21:00:32 -0500

    Funcionamiento Juego
    El juego funciona hasta el final de este. Pendiente redibujar botones.

--- js/juego.js
+++ js/juego.js
@@ -2,13 +2,19 @@ let cabezal = document.querySelector(".puntaje"),
   categoria = document.querySelector("#categoria"),
   preguntaHtml = document.querySelector("#pregunta"),
   puntajeHtml = document.querySelector(".puntaje");
+  contenedor = document.querySelector(".contenedor-respuestas"),
+  
   respuestasHtml = document.querySelector("#respuestas"),
   opciones = respuestasHtml.children;
 
 let preguntasData;
 let categoriasData;
 let ronda = 0;
-let puntaje= 0;
+let puntaje= 100;
+
+let botonOpciones = document.createElement('button')
+botonOpciones.className = "botonOpciones"
+
 
 let aprobadas = [];
 
@@ -39,6 +45,8 @@ const initGame = async () => {
 };
 
 function mostrarCategoria() {
+  contenedor.appendChild(preguntaHtml)
+  respuestasHtml.appendChild(botonOpciones)
   let categoriaSeleccionada = categoriaAlAzar(aprobadas, categoriasData.length);
   aprobadas.push(categoriaSeleccionada);
   categoria.innerHTML = categoriasData[categoriaSeleccionada];
@@ -50,10 +58,32 @@ function mostrarPreguntaCategoria(catIndex) {
   preguntaHtml.innerHTML = preguntaSeleccionada.question;
   for(let index = 0; index < preguntaSeleccionada.choices.length; index++ ){
       opciones[index].innerHTML = preguntaSeleccionada.choices[index];
-      // opciones[index].addEventListener("click", () =>{
-      //   validarRespuesta();
-      // });
+      opciones[index].addEventListener("click", (e)=>{
+        let correctaJugador = e.target.textContent;
+        validarRespuesta(preguntaSeleccionada.choices[preguntaSeleccionada.answer], correctaJugador)
+      })
   } 
+}
+
+function validarRespuesta(correcta, respuestaJp){
+  if(correcta === respuestaJp){
+    ronda++
+    puntaje =+100;
+    console.log(`ronda: ${ronda}`);
+    if(ronda <5){
+      remueve();
+      mostrarCategoria();
+    } else{
+      window.location.replace('/fin.html')
+    }
+  }
+}
+
+function remueve(){
+  const hijos = document.querySelectorAll('#respuestas > *');
+  for ( let c of hijos){
+    c.remove();
+  }
 }
 
 
